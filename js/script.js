33b88e16cd02e0a8dd672d3ed522eed410a37f7c
Micael Macedo <micaelm2009@hotmail.com>
2022-09-26 09:16:57 -0300

    feat: add input address

--- js/script.js
+++ js/script.js
@@ -1,27 +1,86 @@
-// Initialize and add the map
+"use strict";
+const successCallback = (position) => {
+  console.log(position);
+}
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 function initMap() {
-    const CONFIGURATION = {
-        "ctaTitle": "Checkout",
-        "mapOptions": {"center":{"lat": -12.974722,"lng": -38.5124},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":true,"zoom":10,"zoomControl":true,"maxZoom":22,"mapId":""},
-        "mapsApiKey": "YOUR_API_KEY_HERE",
-        "capabilities": {"addressAutocompleteControl":true,"mapDisplayControl":true,"ctaControl":true}
+  const CONFIGURATION = {
+    "ctaTitle": "Checkout",
+    "mapOptions": {"center":{"lat":-12.9704,"lng":-38.5124},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":true,"zoom":14,"zoomControl":true,"maxZoom":22,"mapId":""},
+    "mapsApiKey": "AIzaSyDU5qjAJUDgxXb2JXo-k6bDtcfLWufYwT4",
+    "capabilities": {"addressAutocompleteControl":true,"mapDisplayControl":true,"ctaControl":false}
+  };
+  const componentForm = [
+    'location',
+    'locality',
+    'administrative_area_level_1',
+    'country',
+    'postal_code',
+  ];
+
+  const getFormInputElement = (component) => document.getElementById(component + '-input');
+  const map = new google.maps.Map(document.getElementById("gmp-map"), {
+    zoom: CONFIGURATION.mapOptions.zoom,
+    center: { lat: -12.9704, lng: -38.5124 },
+    mapTypeControl: false,
+    fullscreenControl: CONFIGURATION.mapOptions.fullscreenControl,
+    zoomControl: CONFIGURATION.mapOptions.zoomControl,
+    streetViewControl: CONFIGURATION.mapOptions.streetViewControl
+  });
+  const marker = new google.maps.Marker({map: map, draggable: false});
+  const autocompleteInput = getFormInputElement('location');
+  const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+    fields: ["address_components", "geometry", "name"],
+    types: ["address"],
+  });
+  autocomplete.addListener('place_changed', function () {
+    marker.setVisible(false);
+    const place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert('No details available for input: \'' + place.name + '\'');
+      return;
+    }
+    renderAddress(place);
+    fillInAddress(place);
+  });
+
+  function fillInAddress(place) {  // optional parameter
+    const addressNameFormat = {
+      'street_number': 'short_name',
+      'route': 'long_name',
+      'locality': 'long_name',
+      'administrative_area_level_1': 'short_name',
+      'country': 'long_name',
+      'postal_code': 'short_name',
     };
-    // The location of Uluru
-    const localizacao = { lat: -12.974722, lng: -38.5124 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: CONFIGURATION.mapOptions.zoom,
-        center: { lat:  -12.974722, lng: -38.5124 },
-        mapTypeControl: false,
-        fullscreenControl: CONFIGURATION.mapOptions.fullscreenControl,
-        zoomControl: CONFIGURATION.mapOptions.zoomControl,
-        streetViewControl: CONFIGURATION.mapOptions.streetViewControl
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: localizacao,
-      map: map,
-    });
+    const getAddressComp = function (type) {
+      for (const component of place.address_components) {
+        if (component.types[0] === type) {
+          return component[addressNameFormat[type]];
+        }
+      }
+      return '';
+    };
+    getFormInputElement('location').value = getAddressComp('street_number') + ' '
+              + getAddressComp('route');
+    for (const component of componentForm) {
+      // Location field is handled separately above as it has different logic.
+      if (component !== 'location') {
+        getFormInputElement(component).value = getAddressComp(component);
+      }
+    }
   }
-  
-  window.initMap = initMap;
+
+  function renderAddress(place) {
+    map.setCenter(place.geometry.location);
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    // console.log("Local: Latitude" + place.latitude + "Longitude"+ place.longitude);
+  }
+}
